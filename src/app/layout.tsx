9a56bb07ffac015b46b51fe48ce3c8485013f8c5
Filipe Onoue <filpeflom@gmail.com>
2023-05-25 10:34:41 -0300

    fix: :adhesive_bandage: fix error with 2 body render

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,3 @@
-import NicHeader from "@/components/NicHeader"
-
 import "tailwindcss/tailwind.css";
 
 export default function RootLayout({
@@ -9,8 +7,7 @@ export default function RootLayout({
 }) {
   return (
     <html>
-      <body className="mx-auto my-0 flex flex-col items-center border shadow-md h-full w-[65%]">
-        <NicHeader />
+      <body className="h-screen">
         {children}
       </body>
     </html>
--- src/app/tasklist/layout.tsx
+++ src/app/tasklist/layout.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import NicHeader from "@/components/NicHeader";
-import React from "react";
+import React, { useEffect } from "react";
 import { createContext, useRef } from "react";
 
 import "tailwindcss/tailwind.css";
@@ -13,13 +13,11 @@ export default function TaskListLayout({ children }: { children: React.ReactNode
   const bodyRef = useRef();
 
   return (
-    <html>
-      <body className="mx-auto my-0 flex flex-col items-center border shadow-md h-full w-[65%]" ref={bodyRef}>
-        <NicHeader />
-        <BodyContext.Provider value={{ bodyRef }}>
-          {children}
-        </BodyContext.Provider>
-      </body>
-    </html>
+    <div className="mx-auto my-0 flex flex-col items-center border shadow-md h-full w-[65%] overflow-auto" ref={bodyRef}>
+      <NicHeader />
+      <BodyContext.Provider value={{ bodyRef }}>
+        {children}
+      </BodyContext.Provider>
+    </div>
   )
 }
