1626458a105520f2b65c61a2f4abac8a3f90148f
Filipe Onoue <filpeflom@gmail.com>
2023-05-25 09:55:49 -0300

    feat: :sparkles: import bodyRef to set overflow to auto

--- src/components/DeleteModal.tsx
+++ src/components/DeleteModal.tsx
@@ -1,12 +1,15 @@
-import { TasksContext } from "@/pages/tasklist";
+import { BodyContext } from "@/app/tasklist/layout";
+import { TasksContext } from "@/app/tasklist/page";
 import { useContext } from "react";
 
 export default function DeleteModal({ setIsOpenModal, id }) {
 
   const { dispatch } = useContext(TasksContext);
+  const { bodyRef } = useContext(BodyContext);
 
   const handleCloseModal = () => {
     setIsOpenModal(false);
+    bodyRef.current.style = 'overflow: auto';
   }
 
   const handleDelete = async () => {
@@ -19,13 +22,14 @@ export default function DeleteModal({ setIsOpenModal, id }) {
       })
       const data = await res.json();
       dispatch({ type: 'DELETE_TASK', payload: id })
+      bodyRef.current.style = 'overflow: auto';
     } catch (error) {
       console.log(error);
     }
   }
 
   return (
-    <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] flex items-center justify-center w-screen h-screen bg-black/60">
+    <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] flex items-center justify-center w-full h-full bg-black/60">
       <div className="flex flex-col gap-10 p-5 rounded-md border shadow-md bg-white">
         <span className="m-2 text-lg">Você tem certeza que deseja deletar essa tarefa ?</span>
         <div className="flex gap-4 justify-end items-center">
